39a8c8a84524717166e2d7e4f0f793ec07bcf877
andym1125 <andym1125@gmail.com>
2020-04-08 16:00:15 -0500

    makePost method is more or less finished.

--- postloader.js
+++ postloader.js
@@ -5,9 +5,19 @@ function makePost(args)
 {
     //Creates main post
     var post = document.createElement("div");
+    var test = !!args.picSrc;
+    
+    var image = "";
+    if(!!args.picSrc)
+        image = "<img alt='" + args.picAlt + "' src='" + args.picSrc + "'>";
+    
+    var body = "";
+    if(!!args.postBody)
+        body = args.postBody;
 
     post.innerHTML = "<div class='picture'>" +
-        "<img alt='" + args.picAlt + "' src='" + args.picSrc + "'>" +
+        image +
+        body +
         "</div>" +
         "<div class='comments'>" +
         "Post Comment: " + args.comment +
@@ -18,8 +28,22 @@ function makePost(args)
     document.getElementById(args.feedContainer).appendChild(post);
     
     //Creates the Preview objects
+    var pre = document.createElement("a");
+    pre.href = "#" + args.id;
     
+    pre.innerHTML = "<div class='picture'>" +
+        image +
+        body +
+        "</div>";
     
+    document.getElementById(args.previewContainer).appendChild(pre);
+    
+    //Add to list of posts
     posts.push(args);
+    
+    //Notifies if post will be kept upon reload
+    if(args.userGenned)
+        alert("This post will not be kept if you reload the page. It is for demonstration only.");
+    
     //console.log(posts.join()); //For debug purposes
 }
